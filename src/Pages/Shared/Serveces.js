993c384b5e2d731eb0d0283ea3268703143bc6cd
sheikh-momin <momin.emon14@gmail.com>
2022-11-09 01:43:20 -0800

    show limit data lisdt

--- src/Pages/Shared/Serveces.js
+++ src/Pages/Shared/Serveces.js
@@ -12,8 +12,9 @@ const Serveces = () => {
     <div className='mt-20 mx-4'>
     <h2 className='text-xl font-bold mb-5 text-center'>Our Services</h2>
       {
-        categories.map(category => <Link className=' ' key={categories._id} to='/serves/:id'><p className='py-3 mb-4 text-center rounded bg-primary'>{category.title }</p></Link>)
+        categories.slice(0, 3).map(category => <Link className=' ' key={categories._id} to={`/serves/${category._id}`}><p className='py-3 mb-4 text-center rounded bg-primary'>{category.title }</p></Link>)
       }
+      <Link to='/serves'><p className='py-3 mb-4 text-center rounded bg-primary'>See All</p></Link>
     </div>
   );
 };
