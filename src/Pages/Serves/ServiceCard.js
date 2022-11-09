73b1c8d5e807b9922dd06e09560efe5fbe7bc942
sheikh-momin <momin.emon14@gmail.com>
2022-11-09 08:55:54 -0800

    add photo veiw

--- src/Pages/Serves/ServiceCard.js
+++ src/Pages/Serves/ServiceCard.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const ServiceCard = ({service}) => {
   const { title, img, description, _id }=service
   return (
     <div className="card w-4/6 bg-base-100 shadow-xl m-auto mb-20">
-      <figure><img src={img} alt="" /></figure>
+    
+      <figure>
+        <PhotoProvider
+          speed={() => 800}
+          easing={(type) =>
+            type === 2
+              ? "cubic-bezier(0.36, 0, 0.66, -0.56)"
+              : "cubic-bezier(0.34, 1.56, 0.64, 1)"
+          }
+        >
+          <PhotoView src={img}>
+            <img
+              style={{ objectFit: "cover" }}
+              className=" w-full"
+              src={img}
+              alt="services"
+            />
+          </PhotoView>
+        </PhotoProvider>
+      </figure>
       <div className="card-body">
         <h1 className="card-title text-lime-600 text-2xl">
           {title}
