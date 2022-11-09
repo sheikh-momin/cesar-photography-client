39406942d99022a647444fc7fab653daa6e56597
sheikh-momin <momin.emon14@gmail.com>
2022-11-09 01:20:15 -0800

    show rating views price

--- src/ServiceDetails/ServiceDetails.js
+++ src/ServiceDetails/ServiceDetails.js
@@ -13,8 +13,10 @@ const ServiceDetails = () => {
           </h1>
           <p>{description
           }</p>
-          <div className="card-actions justify-end">
-            
+          <div className="card-actions justify-between text-center mt-5">
+            <p>Ratings: <span className='text-amber-500'>{ratings}</span></p>
+            <p>Views: <span className='text-amber-500'>{views}</span></p>
+            <p>Price: <span className='text-amber-500'>{price}</span></p>
           </div>
         </div>
       </div>
